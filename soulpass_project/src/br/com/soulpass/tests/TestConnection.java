a19012761211e32e8385db7a156dc01f0197abe2
matheusfa08 <littlepacoquinha@gmail.com>
2026-09-02 18:40:33 -0300

    feat: Criada classe dao que irá sustentar a conexão do servidor e posta a teste

--- soulpass_project/src/br/com/soulpass/tests/TestConnection.java
+++ soulpass_project/src/br/com/soulpass/tests/TestConnection.java
@@ -1,4 +1,17 @@
 package br.com.soulpass.tests;
 
+import br.com.soulpass.dao.ConnectionFactory;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class TestConnection {
+    static void main(String[] args){
+        System.out.println("\nTestando conexão com servidor de DataBase");
+        if (ConnectionFactory.obterConexao() != null){
+            System.out.println("\n✅Conectado com sucesso!");
+        } else {
+            System.out.println("\n❎Erro ao conectar!");
+        }
+    }
 }
