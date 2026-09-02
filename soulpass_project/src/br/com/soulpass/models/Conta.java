a091e971cc8a8ff345bf827731d9b24ce6b8cf77
matheusfa08 <littlepacoquinha@gmail.com>
2026-09-02 20:17:04 -0300

    feat: novo item em conta

--- soulpass_project/src/br/com/soulpass/models/Conta.java
+++ soulpass_project/src/br/com/soulpass/models/Conta.java
@@ -3,7 +3,10 @@ package br.com.soulpass.models;
 import br.com.soulpass.interfaces.ConverterParaCredito;
 
 public class Conta implements ConverterParaCredito {
+    private int idConta;
+    private Usuario usuario;
     private double qtdePontos;
+    private double limitePontos;
 
     public double getQtdePontos() {return qtdePontos;}
 
