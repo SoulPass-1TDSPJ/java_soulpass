bc599cfc10100ec546527c0a631a3ed108e6df81
matheusfa08 <littlepacoquinha@gmail.com>
2026-09-04 08:14:33 -0300

    feat: Criando uma classe que se conecta ao Driver de interpretação SQL do OracleDB (JDBC)

--- soulpass_project/src/br/com/soulpass/tests/TestConnection.java
+++ soulpass_project/src/br/com/soulpass/tests/TestConnection.java
@@ -1,4 +1,15 @@
 package br.com.soulpass.tests;
 
+import br.com.soulpass.dao.ConnectionFactory;
+
 public class TestConnection {
+    static void main(){
+        System.out.println("Teste de conexão com o banco de dados...");
+        if(ConnectionFactory.obterConexao() == null){
+            System.out.println("⚠️ Erro ao estabelecer conexão ⚠️");
+        }
+        else{
+            System.out.println("✅Conexão estabelecida✅");
+        }
+    }
 }
